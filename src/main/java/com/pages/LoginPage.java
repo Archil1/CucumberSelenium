7d797e893a125af6d797c075e313abe6b8bd12f2
Archil <archilghosh1@gmail.com>
2024-05-18 23:42:01 +0530

    Updated login page and login page step definition class

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -1,5 +1,39 @@
 package com.pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class LoginPage {
+	
+	private WebDriver driver;
+	//1. by locators private in nature
+	private By emailId=By.id("email");
+	private By password=By.id("passwd");
+	private By signInBtn=By.id("SubmitLogin");
+	private By forgotPwdLink=By.linkText("Forgot your password?111");
+	
+	//2. Constructor of the page class
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+		
+	}
+	
+	//3. page actions :features (behavior) of the page the form of methods
+	//assertions should not be in these class
+	//assertions should be in step definitions or test class
+	public String getLoginPageTitle() {
+		return driver.getTitle();
+	}
+	public boolean isForgotPwdLinkExists() {
+		return driver.findElement(forgotPwdLink).isDisplayed();
+	}
+	public void enterUserName(String username) {
+		driver.findElement(emailId).sendKeys(username);
+	}
+	public void enterPassword(String pwd) {
+		driver.findElement(password).sendKeys(pwd);
+	}
+	public void clickOnLogin() {
+		driver.findElement(signInBtn).click();
+	}
 }
