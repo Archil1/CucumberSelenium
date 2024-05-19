f0ae92bf8574dcf8889952cd4f0f3a02b219a3ff
Archil <archilghosh1@gmail.com>
2024-05-19 14:58:32 +0530

    all changes done. need to figure out the parallel execution plugin

--- src/test/java/testRunners/MyTestRunners.java
+++ src/test/java/testRunners/MyTestRunners.java
@@ -7,7 +7,10 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { ".\\src\\test\\resource\\com\\features" }, glue = { "stepdefinitions", "AppHooks" }, plugin = {
-		"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+		"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","timeline:test-output-thread/"})
 public class MyTestRunners {
 
 }
+/*with cucumber junit parallel execution happens on feature level not scenario level , which means junit will run each feature on a single browser and the scenarios in those feature file will be run one by one (we can solve this issue with cucumber with testng)
+ * 
+ */
